26c8246d247f40d44bc3fdfccfdba1b6768ed185
Austin King <shout@ozten.com>
2010-02-03 01:25:36 -0800

    Roughing out a fix for Issue#4 Sponsor page

--- www/application/controllers/cron.php
+++ www/application/controllers/cron.php
@@ -33,6 +33,62 @@ class Cron_Controller extends Common_Controller {
             echo "Permission Denied";
         }
 	}
+	
+	public function update_sponsors($sekrit)
+	{
+		$this->auto_render = FALSE;
+        if ($this->_isAGood($sekrit)) {
+			$flickr = flickr::makeFlickr();
+			$photos = $flickr->photos_search(array(
+				'user_id' => flickr::userId(),
+				'tags' => 'sponsor',
+			));			
+			foreach($photos['photo'] as $photo) {
+				try {
+					#echo $photo['title'] . ' ';
+					
+					$info = $flickr->photos_getInfo($photo['id']);
+				
+					#echo Kohana::debug($info);
+				
+					#echo $info['description'];
+				
+					$sizes = $flickr->photos_getSizes($photo['id']);
+					#echo "SIZES";
+					#echo Kohana::debug($sizes);
+				
+					$sponsor = ORM::factory('sponsor');
+					$sponsor->flickr_id = $photo['id'];
+					$sponsor->name = $photo['title'];					
+					# This is raw HTML which should contain a link back to the site
+					$sponsor->url = $info['description'];
+					if ($sizes) {
+						foreach($sizes as $size) {
+							/**
+							 * We record Large (usually this is the full size icon
+							 * or Original) since photo is smaller than 500 pixels
+							 */
+							if ('Large' == $size['label'] ||
+								'Original' == $size['label']) {
+								$sponsor->imagesrc = $size['source'];
+								$sponsor->width = $size['width'];
+								$sponsor->height = $size['height'];		
+								break;
+							}
+						}
+					} else {
+						Kohana::log('alert', "No results for photos_getSizes");
+					}
+					$sponsor->save();
+				} catch (Exception $e) {
+					Kohana::log('info', "Caught an exception, coudn't import Flickr image " . $photo['id'] .
+								' ' . $photo['title'] . ' reason: ' . $e);
+					echo Kohana::debug($e);
+				}
+				usleep(300);
+			}
+		}
+	}
     
     private function _isAGood($sekrit)
     {
